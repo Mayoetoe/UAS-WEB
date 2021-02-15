13d4b2dac63be7190fab3d4afc3508ce131ff76c
Mayoetoe <ramawahyupang@gmail.com>
2021-02-15 21:18:57 +0700

    Website

---
+++ footer.php
@@ -0,0 +1,7 @@
+ <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>&copy;Copyright by 18111130_RamaWahyuPangestu_TIFRP18CIDB_UASWEB1</p>
+                </div>
+            </div>
+        </footer> 
